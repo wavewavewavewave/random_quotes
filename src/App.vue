b8c04c1dd0b3a8d814bb013681a5a433ef5cbf3d
wavewavewavewave <nikitashkrabov@gmail.com>
2024-04-12 13:30:21 +0300

    save commit

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,93 @@
 <template>
+  <div class="app">
+    <HeaderComponent title="Random Quotes with Vue.js"/>
+<!--            <QuoteComponent />-->
+    <QuoteComponent :quote="quote"/>
+    <div class="button-container">
+      <button @click="getQuote">NEW QUOTE</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import HeaderComponent from "@/components/HeaderComponent";
+import QuoteComponent from "@/components/QuoteComponent";
+
+export default {
+  name: "App",
+  components: {
+    QuoteComponent,
+    HeaderComponent
+  },
+  data() {
+    return {
+      quote: {
+        text: "",
+        author: ""
+
+      },
+      quotes: []
+    };
+  },
+  created() {
+    this.getQuote();
+  },
+  methods: {
+    async getQuote() {
+      const data = await fetch("https://type.fit/api/quotes").then(res => res.json());
+      const randomQuote = Math.floor(Math.random() * data.length);
+
+      this.quote = {
+        text: data[randomQuote].text,
+        author: data[randomQuote].author
+      };
+    }
+  }
+};
+</script>
+
+<style>
+:root {
+  --primary: #42b883;
+  --secondary: #35495e;
+  --dark: #1a1a1a;
+  --lite: #e1e1e1;
+  --gray: #2f2f2f;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: #f8a7b6;
+}
+
+.button-container {
+  display: flex;
+  justify-content: center;
+  padding: 0 20px;
+  margin: 40px auto;
+}
+
+button {
+  color: var(--primary);
+  border: none;
+  background-color: var(--secondary);
+  padding: 10px 20px;
+  font-size: 22px;
+  border-radius: 20px;
+  cursor: pointer;
+  transition: 0.4s;
+}
+
+button:hover {
+  background-color: var(--gray);
+}
+</style>
+
+
+<!--<template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
@@ -23,4 +112,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style>-->
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,13 +1,29 @@
 <template>
-$END$
+  <header>
+<!--    <h1>{{ title }}</h1>-->
+    <h1>Random Quotes with Vue.js</h1>
+  </header>
 </template>
 
 <script>
 export default {
-name: "HeaderComponent"
-}
+  props: ["title"],
+};
 </script>
 
 <style scoped>
-
+header {
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+}
+h1 {
+  font-size: 36px;
+  color: var(--dark);
+  font-weight: 700;
+  text-transform: uppercase;
+  margin-bottom: 10px;
+  border-bottom: 5px solid var(--primary);
+  text-align: center;
+}
 </style>
